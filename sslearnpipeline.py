4af9c2e7aa5c92aff264d4ab4462842ff21c94b4
David Schneider <davidsch@slac.stanford.edu>
2017-02-13 12:50:09 -0800

    sketch lib

--- sslearnpipeline.py
+++ sslearnpipeline.py
@@ -1,3 +1,14 @@
 class SSLearnPipeline(object):
-  pass
+  def __init__(self, outputdir):
+    self.outpudir = outputdir
+    
+  def label(self, img, keystr):
+    # decide on the output name
+    # construct the command line to run labelme
+    # look at the output afterwards
+    pass
+  
+  
+
+  
   
